e2f4ebe4413eb9c42ba3e151a5699299905702cf
Coey Chan <coey.wy.chan@gmail.com>
2024-04-19 01:07:58 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'jquery/dist/jquery.min';
+import 'bootstrap/dist/js/bootstrap.min';
+import './assets/css/style.css';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import ClimateLens from './climateLens';
+
+function App() {
+    return(<Script/>)
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
